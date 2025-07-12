8096c9adb4d6e4dd0f2e0779ec5277c74ae39ec5
AhmedEmadAE <ahmeddace8@gmail.com>
2025-07-12 21:49:21 +0300

    Update page.jsx

--- src/app/Gallery/page.jsx
+++ src/app/Gallery/page.jsx
@@ -5,12 +5,14 @@ export default function Gallery() {
     return (
         <div>
             <h1>Gallery</h1>
-            <ul style={{ fontSize: "30px", color: "blue" }}>
+            <ul style={{
+                color: 'red'
+            }}>
                 <li>
-                    <Link href="/Gallery/web">Web Gallery</Link>
+                    <Link href="/Gallery/web"><h1>Web Gallery</h1></Link>
                 </li>
                 <li>
-                    <Link href="/Gallery/mobile">Mobile Gallery</Link>
+                    <Link href="/Gallery/mobile"><h1>Mobile Gallery</h1></Link>
                 </li>
             </ul>
         </div>
